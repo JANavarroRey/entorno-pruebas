b4d4e2b32ffc4fcfb3a7103523a5cca8cff2211b
JOSE ANTONIO NAVARRO REY | INFORGES | <joseantonio.navarro@inforges.es>
2026-06-25 09:39:55 +0200

    Limpieza del proyecto y añadir .gitignore

--- wp-templates-export/src/js/animations/gsap-setup.js
+++
@@ -1,9 +0,0 @@
-export function initGSAP() {
-  if (typeof gsap === 'undefined' || typeof ScrollTrigger === 'undefined') {
-    console.warn('GSAP or ScrollTrigger not loaded via CDN');
-    return false;
-  }
-  
-  gsap.registerPlugin(ScrollTrigger);
-  return true;
-}
